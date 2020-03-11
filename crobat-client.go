ef663dd8d32f922b018b82b2427c0f17d293021b
pry <pry@Ghostbook-Air.home>
2020-03-11 23:25:44 +0000

    updated

--- crobat-client.go
+++ crobat-client.go
@@ -37,23 +37,49 @@ func get_all(domain string, api_server_host string, api_server_port string) []ma
 	return data
 }
 
-
 func main() {
+	api_host := "192.168.42.56"
+	api_port := "1337"
 	domain_sub := flag.String("s", "", "Get subdomains for this value")
 	domain_tld := flag.String("t", "", "Get tlds for this value")
 	domain_all := flag.String("all", "", "Get all data for this query")
 
+	format := flag.String("f", "plain", "Set output format (json/plain)")
+
 	flag.Parse()
 
 	if (*domain_sub != "") {
-		str, _ := json.MarshalIndent(get_subdomains(*domain_sub, "192.168.42.56", "1337"), "", "    ")
-		fmt.Println(string(str))
+		if (*format == "json") {
+			str, _ := json.MarshalIndent(get_subdomains(*domain_sub, api_host, api_port), "", "    ")
+			fmt.Println(string(str))
+		} else if (*format == "plain") {
+			subdomains := get_subdomains(*domain_sub, api_host, api_port)
+			for i := 0; i < len(subdomains); i++ {
+				fmt.Println(subdomains[i])
+			}
+		}
+		
 	} else if (*domain_tld != "") {
-		str, _ := json.MarshalIndent(get_tlds(*domain_tld, "192.168.42.56", "1337"), "", "    ")
-		fmt.Println(string(str))
+		if (*format == "json") {
+			str, _ := json.MarshalIndent(get_tlds(*domain_tld, api_host, api_port), "", "    ")
+			fmt.Println(string(str))
+		} else if (*format == "plain") {
+			tlds := get_tlds(*domain_tld, api_host, api_port)
+			for i := 0; i < len(tlds); i++ {
+				fmt.Println(tlds[i])
+			}
+		}
+
 	} else if (*domain_all != "") {
-		str, _ := json.MarshalIndent(get_all(*domain_all, "192.168.42.56", "1337"), "", "    ")
-		fmt.Println(string(str))
+		if (*format == "json") {
+			str, _ := json.MarshalIndent(get_all(*domain_all, api_host, api_port), "", "    ")
+			fmt.Println(string(str))
+		} else if (*format == "plain") {
+		
+			for _, i := range get_all(*domain_all, api_host, api_port) {
+				fmt.Println(i["name"])
+			}
+		}
 	}
 
 }
